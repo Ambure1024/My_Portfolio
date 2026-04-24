ace1125fe34ae297ad872f350b4720032ddf6622
Komal <komalambure40@gmail.com>
2026-04-24 21:41:20 +0530

    Add files via upload

---
+++ my_portfolio/cdn.counter.dev/script.js
@@ -0,0 +1,1 @@
+No Content: https://cdn.counter.dev/script.js
---
+++ my_portfolio/komal10-portfolio.vercel.app/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+// function openNewWindow(){
+//     window.open("file:///C:/Users/ambur/Downloads/ravib45_portfolio.vercel.app%20(main)/ravib45-portfolio.vercel.app/assets/cv.pdf")
+// }
